9212300c0c26f8112729b6b36ed6b0f91106b13e
Paul Hulett <hulett@squareup.com>
2019-07-25 15:32:04 -0700

    debugging for error

--- static/order/index.php
+++ static/order/index.php
@@ -1,18 +1,25 @@
 <?php
 require_once __DIR__ . '/../../vendor/autoload.php';
-$input = file_get_contents('php://input');
-$data = json_decode($input, true);
-$order = new \SquareServerless\Order();
-$order->build($data);
-$response = $order->submit();
-$order = $response->getOrder();
-$charge = new \SquareServerless\Charge();
-$chargeResponse = $charge->chargeOrder($order, $data['nonce']);
-$transaction = $chargeResponse->getTransaction();
-// todo: sort out the actual status from the chargeresponse
-$status = 'SUCCESS';
+try {
+    $input = file_get_contents('php://input');
+    $data = json_decode($input, true);
+    $order = new \SquareServerless\Order();
+    $order->build($data);
+    $response = $order->submit();
+    $order = $response->getOrder();
+    $charge = new \SquareServerless\Charge();
+    $chargeResponse = $charge->chargeOrder($order, $data['nonce']);
+    $transaction = $chargeResponse->getTransaction();
+    $id = $transaction->getId();
+    // todo: sort out the actual status from the chargeresponse
+    $status = 'SUCCESS';
+} catch (\Exception $e) {
+    $status = $e->getMessage();
+} catch (\Throwable $e) {
+    $status = $e->getMessage();
+}
 header("Content-type: application/json");
 echo json_encode([
-    'id' => $transaction->getId(),
+    'id' => $id,
     'status' => $status,
 ]);
